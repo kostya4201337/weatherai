64a946eb9447ad58cbc723624b92fa7323679d98
Alexander <92984863+shipovnikAAA@users.noreply.github.com>
2025-03-03 01:55:57 +0800

    First stable api (routs)

---
+++ backend/models.py
@@ -0,0 +1,231 @@
+import datetime
+import requests
+from geopy.geocoders import Nominatim
+from timezonefinder import TimezoneFinder
+import pytz
+import math
+import pandas as pd
+import openmeteo_requests
+import requests_cache
+from retry_requests import retry
+
+class base():
+    def __init__(self, city, date_from, date_to interval):
+        self.url = "https://api.open-meteo.com/v1/forecast" #url for get data
+        
+        self.geolocator = Nominatim(user_agent="giv_long_latitude") # create geolocator
+        
+        # Setup the Open-Meteo API client with cache and retry on error
+        cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
+        retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+        self.openmeteo = openmeteo_requests.Client(session = retry_session)
+        
+        # local varibles
+        self.city = city
+        self.periods = periods
+        self.interval = interval
+        
+        # geodata
+        location = self.geolocator.geocode(self.city)
+        # check if location found
+        if location:
+            self.latitude = location.latitude
+            self.longitude = location.longitude
+            
+            #object timezone
+            tf = TimezoneFinder()
+            # name of timezone
+            timezone_name = tf.timezone_at(lng=self.longitude, lat=self.latitude)
+            if timezone_name:
+                # object timezone
+                tz = pytz.timezone(timezone_name)
+
+                # now time
+                current_time = datetime.datetime.now(tz)
+
+                # смещение
+                utc_offset_timedelta = current_time.utcoffset()
+
+                # Преобразуем смещение в часы
+                total_seconds = utc_offset_timedelta.total_seconds()
+                utc_offset_hours = total_seconds / 3600
+
+                # Форматируем смещение для вывода (учитываем знак)
+                if utc_offset_hours >= 0:
+                    self.offset_int = utc_offset_hours
+                else:
+                    self.offset_int = -utc_offset_hours
+
+                print(f"Часовой пояс: {timezone_name}")
+                print(f"Смещение от UTC: {self.offset_int}")
+            else:
+                return str("time is not available, check your city and country")
+        else:
+            return str("Местоположение не найдено")
+        
+        print(self.periods[0], self.periods[1])
+        self.periods[0], self.periods[1] = self.periods[0]-datetime.timedelta(hours=-self.offset_int), self.periods[1]-datetime.timedelta(hours = -self.offset_int)
+        print(self.periods[0], self.periods[1])
+        print(datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=3)-datetime.timedelta(hours=-self.offset_int))
+        
+    def __give_prediction__ (self):
+        data_period = self.periods[0]-datetime.timedelta(days = 3)
+        time_difference = self.periods[1] - self.periods[0]
+        print(time_difference.total_seconds())
+        if time_difference.total_seconds() < 0:
+            return str('not correct data period')
+        total_hours = abs(time_difference.total_seconds() / 3600)
+        
+        
+        # if self.periods[0] > datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=3)-datetime.timedelta(hours=-self.offset_int):
+        
+        # Make sure all required weather variables are listed here
+        # The order of variables in hourly or daily is important to assign them correctly below
+        params = {
+            "latitude": self.latitude,
+            "longitude": self.longitude,
+            "hourly": ["temperature_2m", "relative_humidity_2m", "dew_point_2m", "apparent_temperature", "precipitation_probability", "precipitation", "rain", "showers", "snowfall", "snow_depth", "weather_code", "pressure_msl", "surface_pressure", "cloud_cover", "cloud_cover_low", "cloud_cover_mid", "cloud_cover_high", "visibility", "evapotranspiration", "et0_fao_evapotranspiration", "vapour_pressure_deficit", "wind_speed_10m", "wind_speed_80m", "wind_speed_120m", "wind_speed_180m", "wind_direction_10m", "wind_direction_80m", "wind_direction_120m", "wind_direction_180m", "wind_gusts_10m", "temperature_80m", "temperature_120m", "temperature_180m", "soil_temperature_0cm", "soil_temperature_6cm", "soil_temperature_18cm", "soil_temperature_54cm", "soil_moisture_0_to_1cm", "soil_moisture_1_to_3cm", "soil_moisture_3_to_9cm", "soil_moisture_9_to_27cm", "soil_moisture_27_to_81cm"],
+            "start_date": f"{(str(data_period).split( )[0])}",
+            "end_date": f"{self.periods[0].strftime('%Y-%m-%d')}",
+        }
+
+        self.responses = self.openmeteo.weather_api(self.url, params=params)
+        
+        # Process first location. Add a for-loop for multiple locations or weather models
+        r = self.__get_data__()
+        
+        # url = f'https://archive-api.open-meteo.com/v1/archive?latitude={latitude}&longitude={longitude}&start_date={(str(data_period).split( )[0])}&end_date={self.periods[0]}&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,apparent_temperature,precipitation,rain,snowfall,snow_depth,weather_code,pressure_msl,surface_pressure,cloud_cover,cloud_cover_low,cloud_cover_mid,cloud_cover_high,et0_fao_evapotranspiration,vapour_pressure_deficit,wind_speed_10m,wind_speed_100m,wind_direction_10m,wind_direction_100m,wind_gusts_10m,soil_temperature_0_to_7cm,soil_temperature_7_to_28cm,soil_temperature_28_to_100cm,soil_temperature_100_to_255cm,soil_moisture_0_to_7cm,soil_moisture_7_to_28cm,soil_moisture_28_to_100cm,soil_moisture_100_to_255cm&daily='
+        # print(0)
+        # r = requests.get(url)
+        # r = r.json()
+        # df = pd.DataFrame.from_dict(pd.json_normalize(r), orient='columns')
+        # print(r)
+        r.to_csv('adada.csv')
+        # else:
+            
+        #     # Make sure all required weather variables are listed here
+        #     # The order of variables in hourly or daily is important to assign them correctly below
+        #     params = {
+        #     	"latitude": self.latitude,
+        #     	"longitude": self.longitude,
+        #     	"hourly": ["temperature_2m", "relative_humidity_2m", "dew_point_2m", "apparent_temperature", "precipitation_probability", "precipitation", "rain", "showers", "snowfall", "snow_depth", "weather_code", "pressure_msl", "surface_pressure", "cloud_cover", "cloud_cover_low", "cloud_cover_mid", "cloud_cover_high", "visibility", "evapotranspiration", "et0_fao_evapotranspiration", "vapour_pressure_deficit", "wind_speed_10m", "wind_speed_80m", "wind_speed_120m", "wind_speed_180m", "wind_direction_10m", "wind_direction_80m", "wind_direction_120m", "wind_direction_180m", "wind_gusts_10m", "temperature_80m", "temperature_120m", "temperature_180m", "soil_temperature_0cm", "soil_temperature_6cm", "soil_temperature_18cm", "soil_temperature_54cm", "soil_moisture_0_to_1cm", "soil_moisture_1_to_3cm", "soil_moisture_3_to_9cm", "soil_moisture_9_to_27cm", "soil_moisture_27_to_81cm"],
+        #     	"start_date": f"{(str(data_period).split( )[0])}",
+        #     	"end_date": f"{self.periods[0].strftime('%Y-%m-%d')}",
+        #     }
+        #     # return str(self.periods[0])
+        #     self.responses = self.openmeteo.weather_api(self.url, params=params)
+
+        #     # Process first location. Add a for-loop for multiple locations or weather models
+        #     r = self.__get_data__()
+            
+        #     # url = f'https://archive-api.open-meteo.com/v1/archive?latitude={latitude}&longitude={longitude}&past_days={math.ceil((datetime.datetime.now() - data_period).total_seconds()/(24 * 60 * 60))}&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,apparent_temperature,precipitation,rain,snowfall,snow_depth,weather_code,pressure_msl,surface_pressure,cloud_cover,cloud_cover_low,cloud_cover_mid,cloud_cover_high,et0_fao_evapotranspiration,vapour_pressure_deficit,wind_speed_10m,wind_speed_100m,wind_direction_10m,wind_direction_100m,wind_gusts_10m,soil_temperature_0_to_7cm,soil_temperature_7_to_28cm,soil_temperature_28_to_100cm,soil_temperature_100_to_255cm,soil_moisture_0_to_7cm,soil_moisture_7_to_28cm,soil_moisture_28_to_100cm,soil_moisture_100_to_255cm&daily='
+        #     # print(url)
+        #     # r = requests.get(url)
+        #     # r = r.json()
+        #     # df = pd.DataFrame.from_dict(r, orient='columns')
+        #     r.to_csv('adada.csv')
+        #     # print(r)
+        return str(r)
+    
+    def __get_data__(self):
+        """__get_data__ this is function for get data from url
+
+        Args:
+            responses ([data -> self.response]): [setings -> self.response]
+
+        Returns:
+            [dataframe]: [dataframe after get data]
+        """
+        response = self.responses[0]
+        print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+        print(f"Elevation {response.Elevation()} m asl")
+        print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+        print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+        # Process hourly data. The order of variables needs to be the same as requested.
+        hourly = response.Hourly()
+        hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()                                                                     # type: ignore
+        hourly_relative_humidity_2m = hourly.Variables(1).ValuesAsNumpy()                                                               # type: ignore
+        hourly_dew_point_2m = hourly.Variables(2).ValuesAsNumpy()                                                                       # type: ignore
+        hourly_apparent_temperature = hourly.Variables(3).ValuesAsNumpy()                                                               # type: ignore
+        hourly_precipitation = hourly.Variables(4).ValuesAsNumpy()                                                                      # type: ignore
+        hourly_rain = hourly.Variables(5).ValuesAsNumpy()                                                                               # type: ignore
+        hourly_snowfall = hourly.Variables(6).ValuesAsNumpy()                                                                           # type: ignore
+        hourly_snow_depth = hourly.Variables(7).ValuesAsNumpy()                                                                         # type: ignore
+        hourly_weather_code = hourly.Variables(8).ValuesAsNumpy()                                                                       # type: ignore
+        hourly_pressure_msl = hourly.Variables(9).ValuesAsNumpy()                                                                       # type: ignore
+        hourly_surface_pressure = hourly.Variables(10).ValuesAsNumpy()                                                                  # type: ignore
+        hourly_cloud_cover = hourly.Variables(11).ValuesAsNumpy()                                                                       # type: ignore
+        hourly_cloud_cover_low = hourly.Variables(12).ValuesAsNumpy()                                                                   # type: ignore
+        hourly_cloud_cover_mid = hourly.Variables(13).ValuesAsNumpy()                                                                   # type: ignore
+        hourly_cloud_cover_high = hourly.Variables(14).ValuesAsNumpy()                                                                  # type: ignore
+        hourly_et0_fao_evapotranspiration = hourly.Variables(15).ValuesAsNumpy()                                                        # type: ignore
+        hourly_vapour_pressure_deficit = hourly.Variables(16).ValuesAsNumpy()                                                           # type: ignore
+        hourly_wind_speed_10m = hourly.Variables(17).ValuesAsNumpy()                                                                    # type: ignore
+        hourly_wind_speed_100m = hourly.Variables(18).ValuesAsNumpy()                                                                   # type: ignore
+        hourly_wind_direction_10m = hourly.Variables(19).ValuesAsNumpy()                                                                # type: ignore
+        hourly_wind_direction_100m = hourly.Variables(20).ValuesAsNumpy()                                                               # type: ignore
+        hourly_wind_gusts_10m = hourly.Variables(21).ValuesAsNumpy()                                                                    # type: ignore
+        hourly_soil_temperature_0_to_7cm = hourly.Variables(22).ValuesAsNumpy()                                                         # type: ignore
+        hourly_soil_temperature_7_to_28cm = hourly.Variables(23).ValuesAsNumpy()                                                        # type: ignore
+        hourly_soil_temperature_28_to_100cm = hourly.Variables(24).ValuesAsNumpy()                                                      # type: ignore
+        hourly_soil_temperature_100_to_255cm = hourly.Variables(25).ValuesAsNumpy()                                                     # type: ignore
+        hourly_soil_moisture_0_to_7cm = hourly.Variables(26).ValuesAsNumpy()                                                            # type: ignore
+        hourly_soil_moisture_7_to_28cm = hourly.Variables(27).ValuesAsNumpy()                                                           # type: ignore
+        hourly_soil_moisture_28_to_100cm = hourly.Variables(28).ValuesAsNumpy()                                                         # type: ignore
+        hourly_soil_moisture_100_to_255cm = hourly.Variables(29).ValuesAsNumpy()                                                        # type: ignore
+
+        hourly_data = {"date": pd.date_range(
+            start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),                                                              # type: ignore
+            end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),                                                             # type: ignore
+            freq = pd.Timedelta(seconds = hourly.Interval()),                                                                           # type: ignore
+            inclusive = "left"
+        )}
+        hourly_data["temperature_2m"] = hourly_temperature_2m                                                                           # type: ignore
+        hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m                                                               # type: ignore
+        hourly_data["dew_point_2m"] = hourly_dew_point_2m                                                                               # type: ignore
+        hourly_data["apparent_temperature"] = hourly_apparent_temperature                                                               # type: ignore
+        hourly_data["precipitation"] = hourly_precipitation                                                                             # type: ignore
+        hourly_data["rain"] = hourly_rain                                                                                               # type: ignore
+        hourly_data["snowfall"] = hourly_snowfall                                                                                       # type: ignore
+        hourly_data["snow_depth"] = hourly_snow_depth                                                                                   # type: ignore
+        hourly_data["weather_code"] = hourly_weather_code                                                                               # type: ignore
+        hourly_data["pressure_msl"] = hourly_pressure_msl                                                                               # type: ignore
+        hourly_data["surface_pressure"] = hourly_surface_pressure                                                                       # type: ignore
+        hourly_data["cloud_cover"] = hourly_cloud_cover                                                                                 # type: ignore
+        hourly_data["cloud_cover_low"] = hourly_cloud_cover_low                                                                         # type: ignore
+        hourly_data["cloud_cover_mid"] = hourly_cloud_cover_mid                                                                         # type: ignore
+        hourly_data["cloud_cover_high"] = hourly_cloud_cover_high                                                                       # type: ignore
+        hourly_data["et0_fao_evapotranspiration"] = hourly_et0_fao_evapotranspiration                                                   # type: ignore
+        hourly_data["vapour_pressure_deficit"] = hourly_vapour_pressure_deficit                                                         # type: ignore
+        hourly_data["wind_speed_10m"] = hourly_wind_speed_10m                                                                           # type: ignore
+        hourly_data["wind_speed_100m"] = hourly_wind_speed_100m                                                                         # type: ignore
+        hourly_data["wind_direction_10m"] = hourly_wind_direction_10m                                                                   # type: ignore
+        hourly_data["wind_direction_100m"] = hourly_wind_direction_100m                                                                 # type: ignore
+        hourly_data["wind_gusts_10m"] = hourly_wind_gusts_10m                                                                           # type: ignore
+        hourly_data["soil_temperature_0_to_7cm"] = hourly_soil_temperature_0_to_7cm                                                     # type: ignore
+        hourly_data["soil_temperature_7_to_28cm"] = hourly_soil_temperature_7_to_28cm                                                   # type: ignore
+        hourly_data["soil_temperature_28_to_100cm"] = hourly_soil_temperature_28_to_100cm                                               # type: ignore
+        hourly_data["soil_temperature_100_to_255cm"] = hourly_soil_temperature_100_to_255cm                                             # type: ignore
+        hourly_data["soil_moisture_0_to_7cm"] = hourly_soil_moisture_0_to_7cm                                                           # type: ignore
+        hourly_data["soil_moisture_7_to_28cm"] = hourly_soil_moisture_7_to_28cm                                                         # type: ignore
+        hourly_data["soil_moisture_28_to_100cm"] = hourly_soil_moisture_28_to_100cm                                                     # type: ignore
+        hourly_data["soil_moisture_100_to_255cm"] = hourly_soil_moisture_100_to_255cm                                                   # type: ignore
+
+        hourly_dataframe = pd.DataFrame(data = hourly_data)
+        print(hourly_dataframe)
+
+        # Process daily data. The order of variables needs to be the same as requested.
+        # daily = response.Daily()
+        # daily_ = daily.Variables(0).ValuesAsNumpy()
+
+        # daily_data = {"date": pd.date_range(
+        # 	start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
+        # 	end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
+        # 	freq = pd.Timedelta(seconds = daily.Interval()),
+        # 	inclusive = "left"
+        # )}
+        # daily_data[""] = daily_
+
+        # daily_dataframe = pd.DataFrame(data = daily_data)
+        return hourly_dataframe
--- backend/routs.py
+++ backend/routs.py
@@ -1,21 +1,30 @@
-from flask import Flask, request
+from fastapi import FastAPI, HTTPException, status
+# from models import base
+import uvicorn
+import datetime
 
-app = Flask(__name__)
+app = FastAPI(swagger_ui_parameters={"syntaxHighlight": {"theme": "obsidian"}})
 
-@app.route('/', methods=['POST'])
-def main():
-    data = request.get_json()
-    if data == {"button":"readress"}:
-        return reg()
-    return {'main':f'main, {data}'}, 200
+@app.get('/api/prediction')
+async def main(city:str = 'Irkutsk', date_from:str | None = '2020-01-22', date_to:str | None = '2022-01-22', interval:str = 'DAYS', period:str | None = None):
+    # if (date_from and date_to and period) or (not period and (not date_from or not date_to)):
+    if date_from and date_to:
+        if period:
+            raise   HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f'Choose either a period OR a date range (date_from and date_to), but not both.',
+            )
+        else:
+            try:
+                from_date = datetime.datetime.strptime(date_from, '%Y-%m-%d')
+                to_date = datetime.datetime.strptime(date_to, '%Y-%m-%d')
+            except ValueError:
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail=f'Invalid date format. Use YYYY-MM-DD.',
+                )
+    else:
+        return {'ok'}
 
-@app.route('/reg', methods=['GET'])
-def reg():
-    return 'aafafaf'
-
-@app.route('/about')
-def about():
-    pass
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    uvicorn.run("routs:app", host="192.168.0.133", port=5000, reload=True)
